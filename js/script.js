ab2433a84f77b57ef2b8e02348eea0ea6ba81925
Vladimir <wowchik.shadrin@yandex.ru>
2021-05-09 15:27:14 +0300

    homework27_hard

--- js/script.js
+++ js/script.js
@@ -1,49 +1,70 @@
-const filterByType = (type, ...values) => values.filter(value => typeof value === type),
+// описание функций
+// фильтр данных по типу
+const filterByType = (type, ...values) =>
+    //выборка элементов у которых тип равен заданному
+    values.filter((value) => typeof value === type),
+  //очистка результата
+  hideAllResponseBlocks = () => {
+    // массив блоков с результатом
+    const responseBlocksArray = Array.from(
+      document.querySelectorAll("div.dialog__response-block")
+    );
+    // очистка формы: блоки с результатом скрываются
+    responseBlocksArray.forEach((block) => (block.style.display = "none"));
+  },
+  //ф-ция отображения блока с результатом
+  showResponseBlock = (blockSelector, msgText, spanSelector) => {
+    //вызов очистки
+    hideAllResponseBlocks();
+    //отображение выбранного блока
+    document.querySelector(blockSelector).style.display = "block";
+    if (spanSelector) {
+      //вывод значка ок или "error"
+      document.querySelector(spanSelector).textContent = msgText;
+    }
+  },
+  // функции отображения результата
+  showError = (msgText) =>
+    showResponseBlock(".dialog__response-block_error", msgText, "#error"),
+  showResults = (msgText) =>
+    showResponseBlock(".dialog__response-block_ok", msgText, "#ok"),
+  showNoResults = () => showResponseBlock(".dialog__response-block_no-results"),
+  //собственно функция фильтрации данных по типу
+  tryFilterByType = (type, values) => {
+    try {
+      //массив выборки из данных: вызов функции фильтрации и добавление в массив
+      const valuesArray = eval(`filterByType('${type}', ${values})`).join(", ");
+      //если массив не пустой формируется сообщение с данными заданного типа иначе сообщение об отсутствии таких данных
+      const alertMsg = valuesArray.length
+        ? `Данные с типом ${type}: ${valuesArray}`
+        : `Отсутствуют данные типа ${type}`;
+      //вызов функции отображения успешной попытки
+      showResults(alertMsg);
+    } catch (e) {
+      // при вызове исключения сообщение об ошибке
+      showError(`Ошибка: ${e}`);
+    }
+  };
+// кнопка фильтровать
+const filterButton = document.querySelector("#filter-btn");
+//ожидание события нажатия кнопки
+filterButton.addEventListener("click", (e) => {
+  //поля ввода типа и данных
+  const typeInput = document.querySelector("#type");
+  const dataInput = document.querySelector("#data");
 
-	hideAllResponseBlocks = () => {
-		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block'));
-		responseBlocksArray.forEach(block => block.style.display = 'none');
-	},
-
-	showResponseBlock = (blockSelector, msgText, spanSelector) => {
-		hideAllResponseBlocks();
-		document.querySelector(blockSelector).style.display = 'block';
-		if (spanSelector) {
-			document.querySelector(spanSelector).textContent = msgText;
-		}
-	},
-
-	showError = msgText => showResponseBlock('.dialog__response-block_error', msgText, '#error'),
-
-	showResults = msgText => showResponseBlock('.dialog__response-block_ok', msgText, '#ok'),
-
-	showNoResults = () => showResponseBlock('.dialog__response-block_no-results'),
-
-	tryFilterByType = (type, values) => {
-		try {
-			const valuesArray = eval(`filterByType('${type}', ${values})`).join(", ");
-			const alertMsg = (valuesArray.length) ?
-				`Данные с типом ${type}: ${valuesArray}` :
-				`Отсутствуют данные типа ${type}`;
-			showResults(alertMsg);
-		} catch (e) {
-			showError(`Ошибка: ${e}`);
-		}
-	};
-
-const filterButton = document.querySelector('#filter-btn');
-
-filterButton.addEventListener('click', e => {
-	const typeInput = document.querySelector('#type');
-	const dataInput = document.querySelector('#data');
-
-	if (dataInput.value === '') {
-		dataInput.setCustomValidity('Поле не должно быть пустым!');
-		showNoResults();
-	} else {
-		dataInput.setCustomValidity('');
-		e.preventDefault();
-		tryFilterByType(typeInput.value.trim(), dataInput.value.trim());
-	}
+  //проверка на заполнение поля с данными
+  if (dataInput.value === "") {
+    //сообщение пользователю
+    dataInput.setCustomValidity("Поле не должно быть пустым!");
+    //отображение блока пока нечего показывать
+    showNoResults();
+  } else {
+    //сообщение пользователю очитить
+    dataInput.setCustomValidity("");
+    //отмена действий по умолчанию
+    e.preventDefault();
+    //вызов функции фильтрации
+    tryFilterByType(typeInput.value.trim(), dataInput.value.trim());
+  }
 });
-
